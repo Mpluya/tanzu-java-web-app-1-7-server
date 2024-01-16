ecc6236b5e8937cbdf90e622fbb9ea1f134e275d
Mae Large <mlarge@vmware.com>
2024-01-16 06:10:58 -0700

    nudge

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Happy almost Saturday from Spring Boot + Tanzu by VBB!";
+		return "Happy Tuesday from Spring Boot + Tanzu by VBB!";
 	}
 
 }
